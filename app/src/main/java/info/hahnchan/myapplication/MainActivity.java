02485d963f5ca696efc89ef8d31c493bb618d201
Hahn.Chan <porridgec@gmail.com>
2018-05-29 17:24:54 +0800

    0

---
+++ app/src/main/java/info/hahnchan/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package info.hahnchan.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "info.hahnchan.myapplication.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        intent.putExtra(EXTRA_MESSAGE, editText.getText().toString());
+        startActivity(intent);
+    }
+}
